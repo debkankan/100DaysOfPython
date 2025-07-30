1c737609a2b7e39b37c1441db9756170bfb520e8
deb <debkankanbanerjee@gmail.com>
2025-07-30 10:08:44 +0530

    Day14 Files commit

--- Day14/Higher_Lower.py
+++ Day14/Higher_Lower.py
@@ -2,16 +2,15 @@ import art
 from game_data import data
 import random
 
-print(art.logo)
-
 game_on = True
 score = 0
+a = random.choice(data)
+b = random.choice(data)
 
-def higher_lower():
+def higher_lower(a,b):
+    print(art.logo)
     global score
     global game_on
-    a = random.choice(data)
-    b = random.choice(data)
     print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}\n")
     print(art.vs)
     print(f"Against B: {b['name']}, a {b['description']}, from {b['country']}\n")
@@ -20,20 +19,27 @@ def higher_lower():
         if (a['follower_count'] > b['follower_count']):
             score += 1
             print(f"You're right! Current Score: {score}")
+            b = random.choice(data)
+            higher_lower(a,b)
         else:
             print(f"Sorry that was wrong. Final Score: {score}")
             game_on = False
+            return
     else:
         if (a['follower_count'] < b['follower_count']):
             score += 1
             print(f"You're right! Current Score: {score}")
+            a = b
+            b = random.choice(data)
+            higher_lower(a,b)
         else:
             print(f"Sorry that was wrong. Final Score: {score}")
             game_on = False
+            return
 
 
 while game_on:
-    higher_lower()
+    higher_lower(a,b)
 
 
 
