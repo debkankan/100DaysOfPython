d3c328607600d6dccac7f8ea754faf0486cd3fb9
deb <debkankanbanerjee@gmail.com>
2025-08-02 13:17:07 +0530

    Modifying Game Logic.

--- Day17/quiz_brain.py
+++ Day17/quiz_brain.py
@@ -2,20 +2,18 @@ class QuizBrain:
 
     def __init__(self, question_list):
         self.question_number = 1
+        self.score = 0
         self.question_list = question_list
 
     def next_question(self):
+        print('WELCOME TO THE QUIZ GAME....... ')
+        print('\n'*3)
         for item in self.question_list:
             user_choice = input(f"Q.{self.question_number} {item.text} (True/False)?\t").title()
             if(user_choice==item.answer):
                 self.question_number += 1
-                if(self.question_number==13):
-                    print(f'We are at the end of the quiz, your got it all right with final score of: {self.question_number-1}')
-                    break
-                else:
-                    print("Great going, that was correct, we proceed to next question\n")
+                self.score += 1
             else:
-                print(f'You guessed it wrong. Your score is {self.question_number-1}\n')
-                break
+                self.question_number += 1
 
-
+        print(f"\nWe are at the end of the quiz, your final score is: {self.score}/12")
